40fe67b3016af14fdd61725849df01cdb5668a6b
Guilherme Borba <guifborba@hotmail.com>
2025-06-23 20:37:02 -0300

    fix asgi

--- core/asgi.py
+++ core/asgi.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+# Configurar o Django antes de qualquer import que dependa dos modelos
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
